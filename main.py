8ff8f68cf97f02d039d390baf191f07c666512c8
Andrew Paradise <adparadise@gmail.com>
2015-03-25 03:01:33 +0000

    add a blank slate

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from kivy.config import Config
+Config.set('kivy', 'keyboard_mode', 'system')
+
+import kivy
+import math
+kivy.require('1.0.8')
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+
+class Game(Widget):
+    def __init__(self, **kwargs):
+        super(Game, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self, 'text')
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self._keyboard.bind(on_key_up=self._on_keyboard_up)
+    
+    def _on_keyboard_up(self, keyboard, keycode):
+        if keycode[0] == 114:
+            self.isPlayingRequested = False
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[0] == 114:
+            self.isPlayingRequested = True
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def update(self, dt):
+        pass
+
+class MainApp(App):
+    def build(self):
+        game = Game()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
+
+
+if __name__ == '__main__':
+    MainApp().run()
+
